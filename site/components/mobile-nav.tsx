123f8858e00777a9a4d1c9c8bac2bfa54ed7f30f
Aakash N S <aakashns@jovian.com>
2023-10-21 13:18:50 -0700

    remove unused import

--- site/components/mobile-nav.tsx
+++ site/components/mobile-nav.tsx
@@ -6,7 +6,6 @@ import { useRouter } from "next/navigation";
 import { ViewVerticalIcon } from "@radix-ui/react-icons";
 
 import { docsConfig } from "@/config/docs";
-import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
